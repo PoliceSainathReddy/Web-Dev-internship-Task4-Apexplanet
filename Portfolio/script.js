813f013f4e19c63777813ca1217b8631c53d68cf
POLICE SAINATH REDDY <163840648+PoliceSainathReddy@users.noreply.github.com>
2025-07-31 00:33:16 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".contact-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    form.reset();
+  });
+});
